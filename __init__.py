7e83d6cb459a5ccfc2c4a6db6c8e32a5e7f02fb9
Masayuki Ito <m.ito@qu-bit.com>
2016-02-03 11:50:00 +0900

    Release version

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
 
-import os,sys
-sys.path.append(r"C:\Users\ito\.p2\pool\plugins\org.python.pydev_4.5.0.201601121553\pysrc")
-import pydevd
-pydevd.settrace()
+#import os,sys
+#sys.path.append(r"C:\Users\ito\.p2\pool\plugins\org.python.pydev_4.5.0.201601121553\pysrc")
+#import pydevd
+#pydevd.settrace()
 
 def classFactory(iface):
     from PublicTransportFinder import PublicTransportFinder
